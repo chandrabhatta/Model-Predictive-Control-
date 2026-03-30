246ea07b386bd5c6e2c697f7a49d000eb9ea8ada
mathijsvangils118-gif <mathijsvangils118@gmail.com>
2026-03-30 15:54:10 +0200

    Update mpc_controller.m with Q, R and Qf

--- mpc_controller.m
+++ mpc_controller.m
@@ -7,9 +7,9 @@ dx0 = x - xr;
 nx = size(Ad,1);
 nu = size(Bd,2);
 
-Q  = eye(nx);
-R  = 0.1*eye(nu);
-Qf = Q;
+Q  = diag([10, 10, 0.1, 1, 1, 0.1, 5, 1]);
+R  = diag([0.1, 0.1, 0.1, 1, 0.5]);
+Qf = dare(Ad, Bd, Q, R);
 
 [H,f] = build_cost(F,G,dx0,Q,R,Qf,N);
 [Aineq,bineq] = build_constraints(nu,N);
