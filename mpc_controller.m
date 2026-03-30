41979aba9acbd72bd6e1ba4774dd1ebec7b3996e
mathijsvangils118-gif <mathijsvangils118@gmail.com>
2026-03-30 21:40:52 +0200

    Update mpc_controller.m with correct prediction and constraints

--- mpc_controller.m
+++ mpc_controller.m
@@ -1,18 +1,21 @@
-function u = mpc_controller(x, xr, Ad, Bd, N)
+function u = mpc_controller(x, xr, X_ref_win, U_ref_win, Qf)
+
+N = 15;
 
 dx0 = x - xr;
+% Wrapping yaw error
+dx0(7) = atan2(sin(dx0(7)), cos(dx0(7)));
 
-[F,G] = build_prediction(Ad,Bd,N);
+[F,G] = rolloutPrediction(X_ref_win, U_ref_win);
 
 nx = size(Ad,1);
 nu = size(Bd,2);
 
 Q  = diag([10, 10, 0.1, 1, 1, 0.1, 5, 1]);
 R  = diag([0.1, 0.1, 0.1, 1, 0.5]);
-Qf = dare(Ad, Bd, Q, R);
 
 [H,f] = build_cost(F,G,dx0,Q,R,Qf,N);
-[Aineq,bineq] = build_constraints(nu,N);
+[Aineq,bineq] = build_constraints(dx0, X_ref_win, U_ref_win);
 
 options = optimoptions('quadprog','Display','off');
 
