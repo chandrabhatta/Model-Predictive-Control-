7b8780e01f3f3f84e3bee99f6cb2f16ff9475c80
Chandramouli Bhattacharya <chandrabhatta2003@gmail.com>
2026-04-08 21:52:51 +0200

    Update build_cost.m

--- build_cost.m
+++ build_cost.m
@@ -1,17 +1,30 @@
-function [H,f] = build_cost(F,G,x0,Q,R,Qf,N)
+function [H, f] = build_cost(F, G, dx0, Q, R, Qf, U_ref_stack)
 
-nx = size(Q,1);
-nu = size(R,1);
+% Inputs:
+%   F, G          : rollout matrices
+%   dx0           : current deviation x - x_ref
+%   Q, R, Qf      : state/input weight matrices
+%   U_ref_stack   : stacked reference input over horizon
 
-Qbar = kron(eye(N),Q);
-Qbar(end-nx+1:end,end-nx+1:end) = Qf;
+[~, Nnu]  = size(G);
+N  = Nnu / size(R,1);
 
-Rbar = kron(eye(N),R);
 
-H = G'*Qbar*G + Rbar;
-f = G'*Qbar*F*x0;
+% Build block-diagonal weighting matrices
+if N == 1
+    Qbar = Qf;
+else
+    Qbar = blkdiag(kron(eye(N-1), Q), Qf);
+end
+Rbar = kron(eye(N), R);
 
-H = 2*H;
-f = 2*f;
+% Quadratic cost
+H = G' * Qbar * G + Rbar;
+
+% Linear term with reference tracking
+f = G' * Qbar * (F*dx0) - Rbar * U_ref_stack;
+
+% Ensure symmetry of H for quadprog
+H = (H + H')/2;
 
 end
