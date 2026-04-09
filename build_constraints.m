bc27b800e0ec11757c21e844db851bd871138a33
Chandramouli Bhattacharya <chandrabhatta2003@gmail.com>
2026-04-09 16:44:52 +0200

    Update build_constraints.m

--- build_constraints.m
+++ build_constraints.m
@@ -5,20 +5,19 @@ nu = size(U_ref_win,1); % calculate size of input based on argument provided
 N  = size(U_ref_win,2); % calculate the horizon length
 
 % --- Input limits ---
-u_min = [-2; -1; -0.5; -0.3; -0.5];
-u_max = [ 2;  1;  0.5;  0.3;  0.5];
+u_min = [-3; -2; -2; -1; -1];
+u_max = [ 3;  2;  2;  1;  1];
 
 Umin = repmat(u_min, N, 1);
 Umax = repmat(u_max, N, 1);
 
-U_ref_stack = reshape(U_ref_win, [], 1);
 A_u = [eye(N*nu); -eye(N*nu)];
-b_u = [Umax - U_ref_stack; -Umin + U_ref_stack];
+b_u = [Umax; -Umin];
 
 % --- State limits ---
 % Define reasonable limits (adjust as needed)
-x_min = [-Inf; -Inf; -0.1; -2; -2; -0.5; -pi; -pi/6];
-x_max = [ Inf;  Inf; 0.1;  2;  2;  0.5;  pi;  pi/6];
+x_min = [-Inf; -Inf; -Inf; -2; -2; -0.5; -pi; -pi/6];
+x_max = [ Inf;  Inf; Inf;  2;  2;  0.5;  pi;  pi/6];
 
 Xmin = repmat(x_min, N, 1);
 Xmax = repmat(x_max, N, 1);
@@ -31,5 +30,6 @@ b_x = [ Xmax - F*dx0; -Xmin + F*dx0 ];
 
 Aineq = [A_u; A_x];
 bineq = [b_u; b_x];
+
 
 end
